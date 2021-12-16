688c2c99a165bfa5be1e5d96d9cb14d7b875945e
Muhammadamin <example@gmail.com>
2021-12-16 21:50:29 +0500

    Update all folders! But not work because not finished.

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,17 @@
   
 //   exports.default = defaultTask
 
-const {src, dest, watch, parallel} = require('gulp')
-const scss = require('gulp-sass')(require('sass'))
-const browserSync = require('browser-sync').create()
-const autoprefixer = require('gulp-autoprefixer')
-const rename = require("gulp-rename")
-const cleanCSS = require('gulp-clean-css')
-const gcmq = require('gulp-group-css-media-queries')
-const concat = require('gulp-concat')
-const uglify = require('gulp-uglify-es').default;
+const {src, dest, watch, parallel, series} = require('gulp')
+const scss               = require('gulp-sass')(require('sass'))
+const browserSync        = require('browser-sync').create()
+const autoprefixer       = require('gulp-autoprefixer')
+const rename             = require("gulp-rename")
+const cleanCSS           = require('gulp-clean-css')
+const gcmq               = require('gulp-group-css-media-queries')
+const concat             = require('gulp-concat')
+const uglify             = require('gulp-uglify-es').default
+const del                = require('del')
+const imagemin           = 'gulp-imagemin'
 
 // Live serever
 function liveServer(){
@@ -24,6 +26,28 @@ function liveServer(){
         port: 3000,
         notify:false
     })
+}
+
+// Reload dist
+function cleanDist(){
+    return del('dist')
+}
+
+// for Images compile
+function images(){
+   return src('app/image/**/*')   
+        .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+         imagemin.mozjpeg({quality: 75, progressive: true}),
+         imagemin.optipng({optimizationLevel: 5}),
+         imagemin.svgo({
+             plugins: [
+                 {removeViewBox: true},
+                 {cleanupIDs: false}
+             ]
+         })
+     ]))
+     .pipe(dest('dist/image/'))
 }
 
 // Css
@@ -59,6 +83,18 @@ function js(){
     .pipe(browserSync.stream())
 }
 
+// Build
+function build(){
+    return src([
+       'app/css/style.css',
+       'app/css/style.min.css',
+       'app/fonts/*',
+       'app/js/main.min.js',
+       'app/*.html'
+    ]), {base: 'app'}
+    .pipe(dest('dist/'))
+}
+
 
 // Watching
 function watching(){
@@ -73,5 +109,7 @@ exports.style = styles
 exports.watching = watching
 exports.liveServe = liveServer
 exports.js = js
+exports.img = images
 
+exports.build = series(cleanDist, images, build)
 exports.default = parallel(styles, js, watching, liveServer)
